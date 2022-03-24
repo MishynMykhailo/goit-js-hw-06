f32c303c7e98c96afb3e4ed4f28a3808ca41728e
MishynMykhailo <misham18092001@gmail.com>
2022-03-24 17:03:57 +0200

    update 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  divBoxes: document.querySelector("#boxes"),
+  inputNumber: document.querySelector("#controls input"),
+  btnCreate: document.querySelector("button[data-create]"),
+  btnDestroy: document.querySelector("button[data-destroy]"),
+}
+let divSize = 30;
+refs.btnCreate.addEventListener('click',onCreateClick);
+refs.btnDestroy.addEventListener('click',DestroyDiv);
+
+function onCreateClick(){
+  if(refs.inputNumber.value){
+    createBoxes(refs.inputNumber.value)
+  }
+}
+
+function DestroyDiv(){
+  refs.divBoxes.innerHTML = "";
+  divSize = 30;
+}
+
+function createBoxes(amount) {
+  const divArr = [];
+  for( let i =0; i< amount; i+=1){
+    const div = document.createElement('div')
+    div.style.width = divSize + 'px';
+    div.style.height = divSize + 'px';
+    div.style.backgroundColor = getRandomHexColor();
+    divArr.push(div);
+    divSize +=10;
+  }
+  refs.divBoxes.append(...divArr)
+}
